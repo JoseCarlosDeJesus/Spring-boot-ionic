9c7878c45eff74c6442794234ebcdb19938196f8
José Carlos de Jesus Santana Júnior <josecarlosdjesus@outlook.com>
2022-04-28 18:08:46 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/josecarlos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/josecarlos/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.josecarlos.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.josecarlos.cursomc.domain.Categoria;
 import com.josecarlos.cursomc.services.CategoriaService;
@@ -22,15 +26,15 @@ public class CategoriaResource {
 		
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri= ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 		
-//		Categoria cat1= new Categoria(1,"Informatica");
-//		Categoria cat2= new Categoria(2,"Escritório");
-//		
-//		List<Categoria> lista = new ArrayList<>();
-//		lista.add(cat1);
-//		lista.add(cat2);
-//		
-//		return lista;
 	}
 
 }
