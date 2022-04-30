9d2a7d0ec071b0b1adfd1dec2e94cfca61d030a8
José Carlos de Jesus Santana Júnior <josecarlosdjesus@outlook.com>
2022-04-30 18:08:27 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/josecarlos/cursomc/CursomcApplication.java
+++ src/main/java/com/josecarlos/cursomc/CursomcApplication.java
@@ -62,6 +62,8 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		Categoria cat1= new Categoria(null,"Informática");
 		Categoria cat2=new Categoria(null,"Escritório");
+		Categoria cat3=new Categoria(null,"Cama e mesa de banho");
+		Categoria cat4=new Categoria(null,"Macacos de febre");
 		
 		Produto p1=new Produto(null, "Computador",2000.00);
 		Produto p2=new Produto(null, "Impressora",800.00);
@@ -117,7 +119,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getItens().addAll(Arrays.asList(ip3));
 		p3.getItens().addAll(Arrays.asList(ip2));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3, cat4));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
